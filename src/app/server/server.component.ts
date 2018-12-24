bdac8a3502b1ae80bf3aaf12b3f21cfe8147ad30
Michael O'Halloran <C-Michael.OHalloran@charter.com>
2018-12-24 09:09:06 -0700

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  serverId : number;
+  serverStatus: string = this.serverId > 25 ? 'offline' : 'online';
+  allowNewServer : boolean = false;
+  showImage: boolean;
+  imageSource: string;
+
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    },2000);
+    this.imageDisplay();
+    this.getImageSource();
+  }
+
+  ngOnInit() {
+    this.getServerId();
+  }
+
+  toggleServer() {
+    this.allowNewServer = !this.allowNewServer;
+  }
+
+  getServerId() {
+    this.serverId = Math.floor(Math.random() * 50 + 1);
+    return this.serverId;
+  }
+
+  getImageSource() {
+    this.imageSource = 'https://picsum.photos/200/200/?random';
+  }
+
+  //sets boolean randomly
+  imageDisplay() {
+    let decidingFactor = Math.random();
+    this.showImage = decidingFactor > 0.5 ? true : false;
+  }
+
+}
